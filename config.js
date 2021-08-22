10ab252db95f7cff85f9232174568e15c7e0a875
hungxvie <89345142+hungxvie@users.noreply.github.com>
2021-08-22 20:30:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introDesc: `Hưng yêu em bé Viee`,
+    btnIntro: 'Bấm zô đây nè',
+    title: 'I Love U to the moon and back 😙',
+    desc: 'Em yêu anh nhiều không <3 ',
+    btnYes: 'Yêu anh siêu nhiều <3',
+    btnNo: 'Không, Em không yêu anh :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì anh Đẹp Trai,Giỏi,Ngoan,Đặc biệt là rất yêu em <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Sáng mai đi thể dục nhá <3',
+    btnAccept: 'Dạ chồng yêu <3 <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
